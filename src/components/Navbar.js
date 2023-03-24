54f14fc34ef931c3b1af86f630592ff98fd3ac4d
mahendra zala <mahendrazala945@gmail.com>
2023-03-24 13:14:48 +0530

    setalert

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,9 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/"> 
+          <Link className="navbar-brand" to="/"> 
             TextUtils
-          </a>
-          {/* <Link className="navbar-brand" to="/"> 
-            TextUtils
-          </Link> */}
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -25,18 +22,15 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                {/* <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </Link> */}
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
+                </Link>
               </li>
-              {/* <li className="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/about">
                   About
                 </Link>
-              </li> */}
+              </li>
             </ul>
             {/* <form className="d-flex" role="search">
               <input
